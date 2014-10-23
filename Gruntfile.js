c4e578d0bf588fc2ec40edf5173b6add4ff15a61
Guillaume Wuip <guillaume@wuips.com>
2014-10-23 18:39:01 +0200

    No test for a moment

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,11 +30,11 @@ module.exports = function(grunt) {
       },
       lib: {
         files: '<%= jshint.lib.src %>',
-        tasks: ['jshint:lib', 'nodeunit']
+        tasks: ['jshint:lib']
       },
       test: {
         files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'nodeunit']
+        tasks: ['jshint:test']
       },
     },
   });
