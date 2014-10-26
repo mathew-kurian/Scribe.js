c8610e29b05697805fdac04c5dd84ef8894d1f7b
Guillaume Wuip <guillaume@wuips.com>
2014-10-26 11:12:01 +0100

    Add working example of console2 methods

---
+++ examples/console2.js
@@ -0,0 +1,28 @@
+/*jshint -W079 */
+
+/**
+ * Console2 functions
+ */
+
+var scribe  = require('../scribe'), //loads Scribe
+    console = process.console;      //create a local (for the module) console
+
+scribe.config({});
+
+console.addLogger('log');
+
+console.log("Hello");
+
+console.time().log("Print the full time");
+console.date().log("Just print the date");
+console.tag("Tag1", 123).log("Some Tag");
+console.file().log("Print the file and the line of the call");
+console.log({just : 'an object'});
+console.log(
+        "Print many things",
+        { key1 : "val 1", key2 : { a: "b", c : []}, key3 : [1234]},
+        ['an array'],
+        "A String"
+);
+
+console.tag("Combo!").time().file().log("A combo");
